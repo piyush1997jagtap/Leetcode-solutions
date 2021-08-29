a95aba6db3ce7204040e4d92b982deb190b29c90
Piyush <piyush1997jagtap@gmail.com>
2021-08-29 06:16:49 +0000

    Sync LeetCode submission - Reverse Integer (java)

---
+++ problems/reverse_integer/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int MAX = (int)Math.pow(2,31);
+        int MIN = (int)Math.pow(2,-31);
+        int rev = 0;
+        while(x != 0)
+        {
+            int digit = x % 10;
+            x = x/10;
+            
+            if( rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10) && (digit > Integer.MAX_VALUE%10))
+                return 0;
+            if( rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10) && (digit < Integer.MIN_VALUE%10))
+                return 0;
+            rev = rev*10 + digit;
+        }
+                    return rev;
+    }
+}
