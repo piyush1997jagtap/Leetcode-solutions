19cf1518aac9a911ffbb71db17cb86c292f7e23c
Piyush <piyush1997jagtap@gmail.com>
2021-12-19 07:30:14 +0000

    Sync LeetCode submission - Majority Element (java)

---
+++ problems/majority_element/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int count = 0;
+        for(int i =0; i < nums.length; i++)
+        {
+            if(!map.containsKey(nums[i]))
+            {
+                map.put(nums[i], 1);
+                
+            } else {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+            // count = map.getOrDefault(nums[i], 0) + 1;
+            // System.out.println(count);
+        }
+        
+     for(Map.Entry<Integer, Integer> entry: map.entrySet())
+     {
+         if(entry.getValue() > Math.floor(nums.length/2))
+         {
+             return entry.getKey();
+         }
+     }
+
+        return -1;
+    }
+}
