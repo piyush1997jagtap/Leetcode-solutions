5a50ef983bf1871440916cdf8660ecd4e027317f
Piyush <piyush1997jagtap@gmail.com>
2022-01-02 14:34:47 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (java)

--- problems/longest_substring_without_repeating_characters/solution.java
+++ problems/longest_substring_without_repeating_characters/solution.java
@@ -1,22 +1,52 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashSet<Character> set = new HashSet<Character>();
-        int start_pointer = 0;
-        int end_pointer = 0;
-        int maxLength = 0;
-        while(end_pointer < s.length())
+        int n = s.length();
+ 
+//         int res = 0;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j < n; j++) {
+//                 if (checkRepetition(s, i, j)) {
+//                     res = Math.max(res, j - i + 1);
+//                 }
+//             }
+//         }
+ 
+//         return res;
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        
+        while(i < n)
         {
-            if(!set.contains(s.charAt(end_pointer)))
-               {
-                   set.add(s.charAt(end_pointer));
-                   end_pointer++;
-                   maxLength = Math.max(maxLength, set.size());
-               }
-               else {
-                   set.remove(s.charAt(start_pointer));
-                   start_pointer++;
-               }
+            if(!set.contains(s.charAt(i)))
+            {
+               
+                set.add(s.charAt(i));
+                
+                count = Math.max(count, i-j + 1);
+                i++;
+                
+            } else {
+                set.remove(s.charAt(j));
+                j++;
+            }
         }
-               return maxLength;
+        return count;
     }
+    
+    
+//     private boolean checkRepetition(String s, int start, int end) {
+//         int[] chars = new int[128];
+ 
+//         for (int i = start; i <= end; i++) {
+//             char c = s.charAt(i);
+//             System.out.println(c);
+//             chars[c]++;
+//             if (chars[c] > 1) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
 }
